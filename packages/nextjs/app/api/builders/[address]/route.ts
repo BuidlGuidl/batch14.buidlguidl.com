18e00c126141bc1d4b814f4f1d72a997528bf894
phipsae <philip.krause88@yahoo.de>
2025-04-07 12:00:46 +0200

    fix route and swap out fs which doesn't work on vercel

--- packages/nextjs/app/api/builders/[address]/route.ts
+++ packages/nextjs/app/api/builders/[address]/route.ts
@@ -1,14 +1,18 @@
 import { NextResponse } from "next/server";
-import fs from "fs";
+import { readdir } from "fs/promises";
 import path from "path";
 
 export async function GET(request: Request, { params }: { params: { address: string } }) {
+  const buildersPath = path.join(process.cwd(), "app/builders");
   try {
-    const address = params.address;
-    const profilePath = path.join(process.cwd(), "app", "builders", address, "page.tsx");
+    const directories = await readdir(buildersPath, { withFileTypes: true });
+    const address = directories
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name)
+      .filter(name => name.startsWith("0x"))
+      .filter(address => address === params.address);
 
-    // Check if the profile page exists
-    const exists = fs.existsSync(profilePath);
+    const exists = address.length > 0;
 
     return NextResponse.json({ exists });
   } catch (error) {
